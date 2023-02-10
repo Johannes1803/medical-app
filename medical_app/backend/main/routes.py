75270aecf7981fa685f50cedb3cd300bc03c6d77
johannes <johannes.birk11@web.de>
2023-02-10 20:01:11 +0100

    chore (routes): remove obsolete dummy function

--- medical_app/backend/main/routes.py
+++ medical_app/backend/main/routes.py
@@ -4,11 +4,6 @@ from medical_app.backend.main import bp
 from medical_app.backend.models import Medical, Patient
 
 
-@bp.route("/", methods=["GET"])
-def index() -> str:
-    return "Hello world!"
-
-
 @bp.route("/medics/<int:medic_id>", methods=["GET"])
 def get_medic(medic_id) -> Response:
     medic: Medical = Medical.query.get(medic_id)
