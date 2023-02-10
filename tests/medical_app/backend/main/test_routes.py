c6234c6c477c38aa10f45d5d0716458e6503f2e6
johannes <johannes.birk11@web.de>
2023-02-10 20:27:46 +0100

    feat (test): test get medics; reuse code

--- tests/medical_app/backend/main/test_routes.py
+++ tests/medical_app/backend/main/test_routes.py
@@ -1,21 +1,39 @@
 import flask
 
 
-def test_get_medic_should_return_medic(app):
-    # ToDo: Link medic id to test setup
-    res: flask.Response = app.test_client().get("/medics/1")
-
+def assert_success_response_structure(res) -> None:
     assert res.status_code == 200
-
     assert res.json["status"] == "success"
     assert res.json["data"]
 
 
-def test_get_medic_non_existing_should_return_404(app):
+def assert_error_response_structure(res) -> None:
+    assert res.status_code >= 300
+    assert res.json["status"] == "error"
+    assert res.json["message"]
+    assert res.json["code"]
+
+    assert res.status_code == res.json["code"]
+
+
+def test_get_medics_should_return_medics_array(app):
+    res: flask.Response = app.test_client().get("/medics")
+
+    assert_success_response_structure(res)
+
+    assert len(res.json["data"]) > 0
+
+
+def test_get_medic_should_return_medic(app) -> None:
+    # ToDo: Link medic id to test setup
+    res: flask.Response = app.test_client().get("/medics/1")
+
+    assert_success_response_structure(res)
+
+
+def test_get_medic_non_existing_should_return_404(app) -> None:
     res: flask.Response = app.test_client().get("/medics/1000000")
 
     assert res.status_code == 404
 
-    assert res.json["status"] == "error"
-    assert res.json.get("message")
-    assert res.json["code"] == 404
+    assert_error_response_structure(res)
