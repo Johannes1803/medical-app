ec17bf2f37c1b1eb92dd3d436dcd20cf00257fdd
johannes <johannes.birk11@web.de>
2023-02-10 20:00:30 +0100

    feat (tests): add test for get medic

--- tests/medical_app/backend/main/test_routes.py
+++ tests/medical_app/backend/main/test_routes.py
@@ -1,10 +1,21 @@
 import flask
 
 
-def test_get_medics_should_return_list(app):
+def test_get_medic_should_return_medic(app):
+    # ToDo: Link medic id to test setup
     res: flask.Response = app.test_client().get("/medics/1")
 
     assert res.status_code == 200
-    assert res.json["status"] == "success"
 
+    assert res.json["status"] == "success"
     assert res.json["data"]
+
+
+def test_get_medic_non_existing_should_return_404(app):
+    res: flask.Response = app.test_client().get("/medics/1000000")
+
+    assert res.status_code == 404
+
+    assert res.json["status"] == "error"
+    assert res.json.get("message")
+    assert res.json["code"] == 404
