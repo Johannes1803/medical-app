aac8808d8b1e6a6ff1608c4772bd8dc7faae6e76
johannes <johannes.birk11@web.de>
2023-02-10 22:24:24 +0100

    feat (routes): add new test for posting a medic

--- tests/medical_app/backend/main/test_routes.py
+++ tests/medical_app/backend/main/test_routes.py
@@ -1,5 +1,7 @@
 import flask
 
+from medical_app.backend.models import Medical
+
 
 def assert_success_response_structure(res) -> None:
     assert res.status_code == 200
@@ -22,6 +24,38 @@ def test_get_medics_should_return_medics_array(app):
     assert_success_response_structure(res)
 
     assert len(res.json["data"]) > 0
+
+
+def test_post_medic_should_create_new_medic(app):
+    # count medics before creating a new one
+    medics = Medical.query.all()
+    n_medics_before = len(medics)
+
+    # create new medic
+    res = app.test_client().post(
+        "/medics",
+        json={
+            "firstName": "Marc",
+            "lastName": "Tester",
+            "email": "marc.tester@gmail.com",
+            "patients": [],
+        },
+    )
+
+    # test response boilerplate
+    assert_success_response_structure(res)
+
+    # assert the id of the newly created medic is returned
+    new_medic_id = res.json["data"].get("id")
+    assert new_medic_id
+
+    # check new medic is in db
+    assert Medical.query.get(new_medic_id)
+
+    # check medics count increased by one
+    medics = Medical.query.all()
+    n_medics_after = len(medics)
+    assert n_medics_before + 1 == n_medics_after
 
 
 def test_get_medic_should_return_medic(app) -> None:
