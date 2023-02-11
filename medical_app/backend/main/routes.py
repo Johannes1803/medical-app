92d28150dcbaa8b6ea1e77bbe07fb9700f96b14a
johannes <johannes.birk11@web.de>
2023-02-11 14:46:14 +0100

    feat (routes): add delete route

--- medical_app/backend/main/routes.py
+++ medical_app/backend/main/routes.py
@@ -2,7 +2,6 @@ from typing import List
 
 from flask import Response, abort, current_app, jsonify, request
 
-from medical_app.backend import db
 from medical_app.backend.main import bp
 from medical_app.backend.models import Medical, Patient
 
@@ -45,5 +44,21 @@ def get_medic(medic_id) -> Response:
             {
                 "status": "success",
                 "data": medic.format_for_json(),
+            }
+        )
+
+
+@bp.route("/medics/<int:medic_id>", methods=["DELETE"])
+def delete_medic(medic_id) -> Response:
+    medic: Medical = Medical.query.get(medic_id)
+    if not medic:
+        abort(404)
+    else:
+        medic_id = medic.id
+        medic.delete()
+        return jsonify(
+            {
+                "status": "success",
+                "data": medic_id,
             }
         )
