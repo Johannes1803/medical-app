e1b0dc71a0330d8311d98666fbbe49c300816067
johannes <johannes.birk11@web.de>
2023-02-12 17:56:55 +0100

    chore: replace os with pathlib

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import os
+import pathlib
 
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, ".env"))
+basedir: pathlib.Path = pathlib.Path(__file__).parent.resolve()
+load_dotenv(basedir / ".env")
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "medical_app.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(basedir / "medical_app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
