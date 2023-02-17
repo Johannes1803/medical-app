11676841c8efb8d265cbf8f950083967a1f90ea2
johannes <johannes.birk11@web.de>
2023-02-17 22:17:34 +0100

    feat: add helper utility function
    for var naming convention translation

--- medical_app/backend/api_helper_functions.py
+++ medical_app/backend/api_helper_functions.py
@@ -12,3 +12,18 @@ def paginate(collection_: Sequence, offset: int = 0, limit: int = 10) -> Sequenc
     """
     end_index = offset + limit
     return collection_[offset:end_index]
+
+
+def convert_camel_case_to_underscore(camel_case_name: str) -> str:
+    """Convert camel case var name to underscore python convention var name.
+
+    :param camel_case_name: var in camel case
+    :return same var name in under score
+    """
+    under_score_var_name = ""
+    for char in camel_case_name:
+        if char.isupper():
+            under_score_var_name += "_" + char.lower()
+        else:
+            under_score_var_name += char
+    return under_score_var_name
